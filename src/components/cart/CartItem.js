e401055f9041a99f454e95fd3d56a8cf125a4cd4
wingo-dev <rukshanlancer@gmail.com>
2023-04-28 17:12:38 +0800

    cart add, remove button

--- src/components/cart/CartItem.js
+++ src/components/cart/CartItem.js
@@ -1,13 +1,47 @@
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/cart-slice";
+
 const CartItem = (props) => {
-  const cartItem = props.cart;
+  const { id, name, totalPrice, price, quantity } = props.cart;
+  const dispatch = useDispatch();
+
+  const addCart = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        price,
+        name,
+      })
+    );
+  };
+
+  const removeCart = () => {
+    // console.log(props.cart);
+    dispatch(
+      cartActions.removeItemFromCart({
+        id,
+      })
+    );
+  };
+
   return (
     <>
       <tbody>
         <tr>
-          <td>{cartItem.name}</td>
-          <td>${cartItem.price}</td>
-          <td>{cartItem.quantity}</td>
-          <td>${cartItem.totalPrice}</td>
+          <td>{name}</td>
+          <td>${price}</td>
+          <td>
+            <div className="quantity">
+              <button className="quantity-btn" onClick={removeCart}>
+                -
+              </button>
+              <div className="quantity-input">{quantity}</div>
+              <button className="quantity-btn" onClick={addCart}>
+                +
+              </button>
+            </div>
+          </td>
+          <td>${totalPrice}</td>
         </tr>
       </tbody>
     </>
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -31,12 +31,14 @@ const cartSlice = createSlice({
     },
     removeItemFromCart(state, action) {
       const id = action.payload;
-      const existingItem = state.items.find((item) => item.id === id);
-      if (existingItem.quantity === 1) {
-        state.items = state.items.filter((item) => item.id !== id);
+      // const existingItem = state.items.find((item) => item.id === id);
+      console.log(id);
+      console.log(state);
+      if (state.items.quantity === 1) {
+        // state.items = state.items.filter((item) => item.id !== id);
       } else {
-        existingItem.quantity--;
-        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+        // existingItem.quantity--;
+        // existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
