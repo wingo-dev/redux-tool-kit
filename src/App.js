347f06efef484d390bf50bbcc2ce26c621b27dd1
wingo-dev <rukshanlancer@gmail.com>
2023-05-04 14:25:58 +0800

    firebase combine

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import Footer from "./components/footer/Footer";
 import Cart from "./components/cart/Cart";
 import Product from "./components/product/Product";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://react-http-a83c2-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <div>
