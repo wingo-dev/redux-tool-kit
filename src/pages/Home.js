01ee432d44e013414e37bff5632192c7cd1dc4e0
wingo-dev <rukshanlancer@gmail.com>
2023-05-15 14:07:25 +0800

    animated words

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,29 @@
 import "./HomePage.css";
+// import { useSpring, animated } from "react-spring";
+
 const HomePage = () => {
+  // const styles = useSpring({
+  //   from: { opacity: 0 },
+  //   to: { opacity: 1 },
+  //   delay: 1000, // Adjust the delay as needed
+  // });
+
+  const words = ["Welcome", "to", "My", "Website"];
+
   return (
-    <>
-      <div>
-        <div className="video-background">
-          <video autoPlay loop muted>
-            <source src="/bg_video.mp4" type="video/mp4" />
-          </video>
-          <div className="image-overlay">
-            <img src="/pro_pic.jpg" alt="profile" height={100} />
-          </div>
-        </div>
+    <div className="video-background">
+      <video autoPlay loop muted>
+        <source src="/bg_video.mp4" type="video/mp4" />
+      </video>
+      <div className="image-overlay"></div>
+      <div className="hero-title">
+        {words.map((word, index) => (
+          <span key={index} className="hero-word">
+            {word}
+          </span>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
