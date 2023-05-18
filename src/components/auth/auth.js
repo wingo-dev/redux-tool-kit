196527ae921203b3e5e84d47af0580e09a16dd36
wingo-dev <rukshanlancer@gmail.com>
2023-05-18 17:04:01 +0800

    login form searchParam

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -1,12 +1,24 @@
 import "./auth.css";
+import { Link, useSearchParams } from "react-router-dom";
 const Auth = () => {
+  const [searchParams] = useSearchParams();
+  const isLogin = searchParams.get("mode") === "login";
+
   return (
     <div className="login-container">
       <form className="login-form">
-        <h2>Login</h2>
-        <input type="text" name="username" placeholder="Username" />
-        <input type="password" name="password" placeholder="Password" />
-        <button type="submit">Login</button>
+        <h2>{isLogin ? "Login" : "signup"}</h2>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <div>
+          <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
+            {isLogin ? "Login" : "Create a user"}
+          </Link>
+        </div>
       </form>
     </div>
   );
